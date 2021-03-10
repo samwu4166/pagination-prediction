cf42254e52fd79e165e73cbc86c66aaa47a6809d
Sam <sam19970416@gmail.com>
2021-03-10 17:15:12 +0800

    Build fastAPI project

---
+++ app/routers/autopager.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..dependencies import get_token_header
+
+import uuid
+
+from urllib.parse import unquote
+
+router = APIRouter(
+
+    prefix="/autopager",
+
+    tags=["autopager"],
+
+    # dependencies=[Depends(get_token_header)],
+
+    responses={404: {"description": "Not found"}},
+
+)
+
+
+
+fake_items_db = {"test_url1": {"page": ["url1","url2"], "next": ["url1"]}, "test_url2": {"page": ["url1","url2"], "next": ["url1", "url2"]}}
+
+
+
+@router.get("/")
+
+async def read_items():
+    return fake_items_db
+
+
+
+@router.get("/{url_id}")
+
+async def read_item(url_id: str):
+    if url_id not in fake_items_db:
+        raise HTTPException(status_code=404, detail="URL not found")
+    return {"result": fake_items_db[url_id], "url_id": url_id}
+
+
+@router.post("/{url}")
+async def predict_item(url: str):
+    _uid = uuid.uuid1()
+    uncode_url = unquote(url)
+    print("Get post action: ", uncode_url)
+    return {"uuid": _uid, "result": {"page": ["url1","url2"], "next": ["url1"]}}
